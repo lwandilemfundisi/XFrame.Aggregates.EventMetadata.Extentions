95b1a25f064da2b45e2f028b5d2146497eaf8c12
Lwandile Mfundisi <Lwandile.Mfundisi@media24.com>
2023-09-11 14:18:50 +0200

    Add project files.

---
+++ XFrame.Aggregates.EventMetadata.Extentions/DomainContainerMetadataProvidersExtensions.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using XFrame.Aggregates.Events;
+using XFrame.Common.Extensions;
+using XFrame.DomainContainers;
+
+namespace XFrame.Aggregates.EventMetadata.Extentions
+{
+    public static class DomainContainerMetadataProvidersExtensions
+    {
+        public static IDomainContainer AddMetadataProvider<TMetadataProvider>(
+            this IDomainContainer domainContainer)
+            where TMetadataProvider : class, IMetadataProvider
+        {
+            domainContainer
+                .ServiceCollection
+                .AddTransient<IMetadataProvider, TMetadataProvider>();
+
+            return domainContainer;
+        }
+
+        public static IDomainContainer AddMetadataProviders(
+            this IDomainContainer domainContainer,
+            params Type[] metadataProviderTypes)
+        {
+            return domainContainer
+                .AddMetadataProviders((IEnumerable<Type>)metadataProviderTypes);
+        }
+
+        public static IDomainContainer AddMetadataProviders(
+            this IDomainContainer domainContainer,
+            Assembly fromAssembly,
+            Predicate<Type> predicate = null)
+        {
+            predicate = predicate ?? (t => true);
+            var metadataProviderTypes = fromAssembly
+                .GetTypes()
+                .Where(IsMetadataProvider)
+                .Where(t => !t.HasConstructorParameterOfType(IsMetadataProvider))
+                .Where(t => predicate(t));
+            return domainContainer.AddMetadataProviders(metadataProviderTypes);
+        }
+
+        public static IDomainContainer AddMetadataProviders(
+            this IDomainContainer domainContainer,
+            IEnumerable<Type> metadataProviderTypes)
+        {
+            foreach (var t in metadataProviderTypes)
+            {
+                if (t.GetTypeInfo().IsAbstract) continue;
+                if (!t.IsMetadataProvider())
+                {
+                    throw new ArgumentException($"Type '{t.PrettyPrint()}' is not an '{typeof(IMetadataProvider).PrettyPrint()}'");
+                }
+
+                domainContainer.ServiceCollection.AddTransient(typeof(IMetadataProvider), t);
+            }
+            return domainContainer;
+        }
+
+        private static bool IsMetadataProvider(this Type type)
+        {
+            return type.IsAssignableTo<IMetadataProvider>();
+        }
+    }
+}
